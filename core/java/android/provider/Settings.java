901286c01e2dad59dbd4a0bceff2a4b6dfaf28f3
Alex <mazdarider23@gmail.com>
2016-09-12 17:55:33 -0400

    Disable dashboard suggestions [2/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3564,6 +3564,12 @@ public final class Settings {
         };
 
         /**
+         * Disable dashboard suggestions in settings
+         * @hide
+         */
+        public static final String ENABLE_SUGGESTIONS = "enable_suggestions";
+
+        /**
          * IMPORTANT: If you add a new public settings you also have to add it to
          * PUBLIC_SETTINGS below. If the new setting is hidden you have to add
          * it to PRIVATE_SETTINGS below. Also add a validator that can validate
